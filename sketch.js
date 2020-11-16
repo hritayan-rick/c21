e1ff4fe74a62ccbd976f9c24b1ee3d397150106e
hritayan-rick <72493205+hritayan-rick@users.noreply.github.com>
2020-11-16 23:01:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,55 @@
-var fixedRect, movingRect;
-var gameobject1 , gameobject2  , gameobject3 , gameobject4;
-
+var car ,  wall;
+var speed, weight;
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 300,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-  gameobject1 = createSprite(100, 300, 50, 50);
-  gameobject2 = createSprite(100 , 400 , 50, 50);
-  gameobject3 = createSprite(300 , 400 , 50, 50);
-  gameobject4 = createSprite(500 , 400 , 50, 50);
-  movingRect.velocityX = -5;
-  gameobject1.velocityX = +5;
+ createCanvas(800,400);
+
+
+ car1=createSprite(200, 200 , 15, 5);
+ 
+ 
+ car2=createSprite(199, 290 , 15, 5);
+
+ car3=createSprite(199, 50 , 15, 5);
+
+ car4=createSprite(198, 120 , 15, 5);
+  
+ car1 .collidewall1;
+ debug = true;
+
+
+
+ wall1=createSprite(700, 200, 12, 56)
+ wall1.shapeColor = (80 , 80 , 80);
+
+ wall2=createSprite(702, 280, 12, 56)
+ wall2.shapeColor = (80 , 80 , 80);
+
+ wall3=createSprite(700, 50, 12, 56)
+ wall3.shapeColor = (80 , 80 , 80);
+
+ wall4=createSprite(700, 130, 12, 56)
+ wall4.shapeColor = (80 , 80 , 80);
+
+
+ car1.velocityX = 5;
+ car1.speed=2;
+ weight=random(401, 1501)
+
+ car2.velocityX = 5;
+ car1.speed=2;
+ weight=random(401, 1501)
+  
+ car3.velocityX = 5;
+ car1.speed=2;
+ weight=random(401, 1501)
+
+ car4.velocityX = 5;
+ car1.speed=2;
+ weight=random(401, 1501)
+
 }
 
 function draw() {
-  background(0,0,0);  
-  bounceoff(gameobject1 , movingRect);
-  
+  background(0 , 255 ,0);  
   drawSprites();
 }
